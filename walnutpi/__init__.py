425c7d5c1e572af4e4225524090dec901bc6b146
sc-bin <3335447573@qq.com>
2026-01-06 11:47:00 +0800

    add support for walnutpi_isp

--- walnutpi/__init__.py
+++ walnutpi/__init__.py
@@ -2,3 +2,5 @@ __model = open("/proc/device-tree/model").read()
 if "walnutpi-2b" in __model:
     from walnutpi_npu import NPU
     from walnutpi_npu import YOLO11
+    import walnutpi_isp as isp
+    
