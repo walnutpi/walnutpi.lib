8ad190f976c15c449a29ecff34095a612517eaf7
sc-bin <3335447573@qq.com>
2026-03-16 16:35:21 +0800

    [1.1.0] 增加对k230的kpu库的引用

--- walnutpi/__init__.py
+++ walnutpi/__init__.py
@@ -4,3 +4,7 @@ if "walnutpi-2b" in __model:
     from walnutpi_npu import YOLO11
     import walnutpi_isp as isp
     
+if "K230" in __model:
+    from walnutpi_kpu import nncase_2_10
+    from walnutpi_kpu import nncase_2_11
+    from walnutpi_kpu import YOLO11
