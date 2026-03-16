e1a52c11295d9f338533267adbc72664e226004c
sc-bin <3335447573@qq.com>
2026-03-16 17:43:55 +0800

    修复k230引用时nncase版本冲突问题

--- walnutpi/__init__.py
+++ walnutpi/__init__.py
@@ -5,6 +5,5 @@ if "walnutpi-2b" in __model:
     import walnutpi_isp as isp
     
 if "K230" in __model:
-    from walnutpi_kpu import nncase_2_10
-    from walnutpi_kpu import nncase_2_11
     from walnutpi_kpu import YOLO11
+    import walnutpi_kpu as kpu
