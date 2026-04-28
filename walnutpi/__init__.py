b3c6414f99f9f6aaebbc9799560501983f7ab8c3
sc-bin <3335447573@qq.com>
2026-04-28 16:09:10 +0800

    修改k230板子的model名称

--- walnutpi/__init__.py
+++ walnutpi/__init__.py
@@ -4,6 +4,6 @@ if "walnutpi-2b" in __model:
     from walnutpi_npu import YOLO11
     import walnutpi_isp as isp
     
-if "K230" in __model:
+if "CyberCAM" in __model:
     from walnutpi_kpu import YOLO11
     import walnutpi_kpu as kpu
